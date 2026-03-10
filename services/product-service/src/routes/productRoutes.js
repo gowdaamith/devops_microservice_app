409664b412ec172598e8b343e972c4f58c640136
gowdaamith <amithgowda1772@gmail.com>
2026-03-10 20:15:30 +0530

    Create productRoutes.js

---
+++ services/product-service/src/routes/productRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const router = express.Router()
+
+const {
+ createProduct,
+ getProducts,
+ deleteProduct
+} = require("../controllers/productController")
+
+router.post("/", createProduct)
+router.get("/", getProducts)
+router.delete("/:id", deleteProduct)
+
+module.exports = router

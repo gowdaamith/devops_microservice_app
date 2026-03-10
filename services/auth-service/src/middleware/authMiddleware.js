761612b16682a6479c2d5b97ce26ab82ba332fa1
gowdaamith <amithgowda1772@gmail.com>
2026-03-10 19:04:13 +0530

    Create authMiddleware.js

---
+++ services/auth-service/src/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken")
+
+module.exports = (req, res, next) => {
+
+  const token = req.headers["authorization"]
+
+  if (!token) {
+    return res.status(401).json({ message: "Token missing" })
+  }
+
+  try {
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || "secret"
+    )
+
+    req.user = decoded
+
+    next()
+
+  } catch (err) {
+
+    return res.status(403).json({ message: "Invalid token" })
+
+  }
+}

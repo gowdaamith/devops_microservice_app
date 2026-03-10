2bc0b40e57b1cebc3ec318dd71ec0fe1fc0e2798
gowdaamith <amithgowda1772@gmail.com>
2026-03-10 19:02:04 +0530

    Create mongoClient.js

---
+++ services/auth-service/src/db/mongoClient.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+  try {
+
+    await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://localhost:27017/authdb"
+    )
+
+    console.log("MongoDB Connected")
+
+  } catch (err) {
+
+    console.error("MongoDB Connection Failed", err)
+    process.exit(1)
+
+  }
+}
+
+module.exports = connectDB

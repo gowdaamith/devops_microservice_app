3140fb77b998424026c4dfb9a69a84f51183a631
gowdaamith <amithgowda1772@gmail.com>
2026-03-10 20:16:49 +0530

    Create mongoClinet.js

---
+++ services/product-service/src/db/mongoClinet.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+
+ try{
+
+  await mongoose.connect(
+   process.env.MONGO_URI || "mongodb://localhost:27017/productdb"
+  )
+
+  console.log("MongoDB Connected")
+
+ }catch(err){
+
+  console.error(err)
+  process.exit(1)
+
+ }
+
+}
+
+module.exports = connectDB
